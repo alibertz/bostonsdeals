b52439c29e189203bdc569cb4ec154477f94ce9c
alibertz <kevinaliberti@gmail.com>
2019-12-12 21:00:40 -0500

    production build of app

---
+++ client/build/precache-manifest.f03cf4ea7637bd4070130d4e807f2e85.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d774c443e5146777edd6dfe668e5bca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce7de6779d06dfb38a96",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "d044752f88b62f519df2",
+    "url": "/static/css/main.d64d6741.chunk.css"
+  },
+  {
+    "revision": "ce7de6779d06dfb38a96",
+    "url": "/static/js/2.59e2fc91.chunk.js"
+  },
+  {
+    "revision": "0c08a48b164499df78c0607b14eb2c76",
+    "url": "/static/js/2.59e2fc91.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d044752f88b62f519df2",
+    "url": "/static/js/main.5348e6c4.chunk.js"
+  },
+  {
+    "revision": "22f671bcb0151be86718",
+    "url": "/static/js/runtime-main.188d3272.js"
+  }
+]);
