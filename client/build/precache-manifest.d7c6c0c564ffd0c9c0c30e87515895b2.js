5faad2b74bdb662b26827ef4674884e5bbd4a5c7
alibertz <kevinaliberti@gmail.com>
2019-12-12 22:25:19 -0500

    bugs

---
+++ client/build/precache-manifest.d7c6c0c564ffd0c9c0c30e87515895b2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a74c57d252fcad3328657707c1c180c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ce7de6779d06dfb38a96",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "16915f0cc49455bc10ff",
+    "url": "/static/css/main.d64d6741.chunk.css"
+  },
+  {
+    "revision": "ce7de6779d06dfb38a96",
+    "url": "/static/js/2.59e2fc91.chunk.js"
+  },
+  {
+    "revision": "0c08a48b164499df78c0607b14eb2c76",
+    "url": "/static/js/2.59e2fc91.chunk.js.LICENSE"
+  },
+  {
+    "revision": "16915f0cc49455bc10ff",
+    "url": "/static/js/main.325b636f.chunk.js"
+  },
+  {
+    "revision": "22f671bcb0151be86718",
+    "url": "/static/js/runtime-main.188d3272.js"
+  }
+]);
