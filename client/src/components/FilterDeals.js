e64867fd40c950e842928879cc1660a5c0141eb4
alibertz <kevinaliberti@gmail.com>
2020-04-22 14:28:51 -0400

    feature tweaks

--- client/src/components/FilterDeals.js
+++ client/src/components/FilterDeals.js
@@ -4,118 +4,104 @@ import AutoCompleteText from './AutoCompleteText';
 const INPUT_TIMEOUT = 250;
 
 class FilterDeals extends Component {
-    
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: '',
-            predictions: []
-        };
-        this.onChange = this.onChange.bind(this);
-    }
+		
+		constructor(props) {
+				super(props);
+				this.state = {
+						value: '',
+						predictions: []
+				};
+				this.onChange = this.onChange.bind(this);
+		}
 
-    getPredictions(value) {
+		getPredictions(value) {
 
-        return [
-            'Fenway/Kenmore',
-            'Back Bay',
-            'Brighton',
-            'North End',
-            'South End',
-            'Cambridge',
-            'Somerville',
-            'Financial District',
-            'Harvard Square',
-            'Copley Square'
-        ].filter(item => item.toLowerCase().indexOf(value.toLowerCase()) !== -1);
-    }
+				return [
+						'Fenway/Kenmore',
+						'Back Bay',
+						'Brighton',
+						'North End',
+						'South End',
+						'Cambridge',
+						'Somerville',
+						'Financial District',
+						'Harvard Square',
+						'Copley Square'
+				].filter(item => item.toLowerCase().indexOf(value.toLowerCase()) !== -1);
+		}
 
-    onChange(e) {
-        // clear timeout when input changes value
-        clearTimeout(this.timeout);
-        const value = e.target.value;
-        this.setState({
-          value: value
-        });
-        // console.log(this.state.value);
-        // console.log(this.state.predictions);
+		onChange(e) {
+				// clear timeout when input changes value
+				clearTimeout(this.timeout);
+				const value = e.target.value;
+				this.setState({
+					value: value
+				});
+				// console.log(this.state.value);
+				// console.log(this.state.predictions);
 
-        if (value.length > 0) {
-          // make delayed api call
-          this.timeout = setTimeout(() => {
-            const predictions = this.getPredictions(value);
-            this.setState({
-              predictions
-            });
-          }, INPUT_TIMEOUT);
-        } else {
-          this.setState({
-            predictions: []
-          });
-        }
-    }
+				if (value.length > 0) {
+					// make delayed api call
+					this.timeout = setTimeout(() => {
+						const predictions = this.getPredictions(value);
+						this.setState({
+							predictions
+						});
+					}, INPUT_TIMEOUT);
+				} else {
+					this.setState({
+						predictions: []
+					});
+				}
+		}
 
-    render() {
-        return (
-            <div id="filterDeals">
+		render() {
+				return (
+						<div id="filterDeals">
 
-                <AutoCompleteText 
-                    handleSelect={this.props.handleSelect} 
-                    handleChange={this.props.handleChange}
-                    suggestions={this.props.suggestions}
-                    onTextChanged={this.props.onTextChanged}
-                    value={this.props.value}
-                    posts={this.props.posts}
-                    />
-        
-                {/*<div id="dayOfWeekSelector" className="dropdown show">
-                    <a className="btn btn-secondary dropdown-toggle" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" onKeyPress={this.props.setPosts}>
-                        {this.props.dayOfWeek}
-                    </a>
-                     <div name="dayOfWeek" className="dropdown-menu" aria-labelledby="dropdownMenuLink">
-                        <option className="dropdown-item" onClick={this.props.handleDayChange}  name="dayOfWeek" value="Sunday">Sunday</option>
-                        <option className="dropdown-item" onClick={this.props.handleDayChange}  name="dayOfWeek" value="Monday">Monday</option>
-                        <option className="dropdown-item" onClick={this.props.handleDayChange}  name="dayOfWeek" value="Tuesday">Tuesday</option>
-                        <option className="dropdown-item" onClick={this.props.handleDayChange}  name="dayOfWeek" value="Wednesday">Wednesday</option>
-                        <option className="dropdown-item" onClick={this.props.handleDayChange}  name="dayOfWeek" value="Thursday">Thursday</option>
-                        <option className="dropdown-item" onClick={this.props.handleDayChange}  name="dayOfWeek" value="Friday">Friday</option>
-                        <option className="dropdown-item" onClick={this.props.handleDayChange}  name="dayOfWeek" value="Saturday">Saturday</option>
-                    </div> */}
-                    
-                    <div id="dayOfWeekSelector">
-                      <label>
-                        <input onClick={this.props.handleDayChange} type="radio" name="dayOfWeek" value="Sunday"/>
-                        <span>Sun</span>
-                      </label>
-                      <label>
-                        <input onClick={this.props.handleDayChange} type="radio" name="dayOfWeek" value="Monday"/>
-                        <span>Mon</span>
-                      </label>
-                      <label>
-                        <input onClick={this.props.handleDayChange} type="radio" name="dayOfWeek" value="Tuesday"/>
-                        <span>Tues</span>
-                      </label>
-                      <label>
-                        <input onClick={this.props.handleDayChange} type="radio" name="dayOfWeek" value="Wednesday"/>
-                        <span>Wed</span>
-                      </label>
-                      <label>
-                        <input onClick={this.props.handleDayChange} type="radio" name="dayOfWeek" value="Thursday"/>
-                        <span>Thu</span>
-                      </label>
-                      <label>
-                        <input onClick={this.props.handleDayChange} type="radio" name="dayOfWeek" value="Friday"/>
-                        <span>Fri</span>
-                      </label>
-                      <label>
-                        <input onClick={this.props.handleDayChange} type="radio" name="dayOfWeek" value="Saturday"/>
-                        <span>Sat</span>
-                      </label>  
-                    </div>
-                </div>
-        // </div>
-        )
-    }
+								<AutoCompleteText 
+										handleSelect={this.props.handleSelect} 
+										handleChange={this.props.handleChange}
+										suggestions={this.props.suggestions}
+										onTextChanged={this.props.onTextChanged}
+										value={this.props.value}
+										posts={this.props.posts}
+										/>
+										
+										<div id="dayOfWeekSelector">
+											<label>
+												<input onClick={this.props.handleDayChange} type="radio" name="dayOfWeek" value="Sunday" defaultChecked={this.props.dayOfWeek === 'Sunday'}/>
+												<span>Sun</span>
+											</label>
+											<label>
+												<input onClick={this.props.handleDayChange} type="radio" name="dayOfWeek" value="Monday" defaultChecked={this.props.dayOfWeek === 'Monday'}/>
+												<span>Mon</span>
+											</label>
+											<label>
+												<input onClick={this.props.handleDayChange} type="radio" name="dayOfWeek" value="Tuesday" defaultChecked={this.props.dayOfWeek === 'Tuesday'}/>
+												<span>Tues</span>
+											</label>
+											<label>
+												<input onClick={this.props.handleDayChange} type="radio" name="dayOfWeek" value="Wednesday" defaultChecked={this.props.dayOfWeek === 'Wednesday'}/>
+												<span>Wed</span>
+											</label>
+											<label>
+												<input onClick={this.props.handleDayChange} type="radio" name="dayOfWeek" value="Thursday" defaultChecked={this.props.dayOfWeek === 'Thursday'}/>
+												<span>Thu</span>
+											</label>
+											<label>
+												<input onClick={this.props.handleDayChange} type="radio" name="dayOfWeek" value="Friday" defaultChecked={this.props.dayOfWeek === 'Friday'}/>
+												<span>Fri</span>
+											</label>
+											<label>
+												<input onClick={this.props.handleDayChange} type="radio" name="dayOfWeek" value="Saturday" defaultChecked={this.props.dayOfWeek === 'Saturday'}/>
+												<span>Sat</span>
+											</label>  
+										</div>
+								</div>
+				// </div>
+				)
+		}
 }
 
 export default FilterDeals;
--- client/src/components/PostViewer.js
+++ client/src/components/PostViewer.js
@@ -7,90 +7,85 @@ import { Spring } from "react-spring/renderprops";
 import Map from './Map';
 
 const GET_POSTS = gql`
-  query GET_POSTS($location: String, $dayOfWeek: String){
-    posts(location: $location dayOfWeek: $dayOfWeek) {
-      id
-      location
-      description
-      companyName
-      lat
-      lng
-    }
-  }
+	query GET_POSTS($location: String, $dayOfWeek: String){
+		posts(location: $location dayOfWeek: $dayOfWeek) {
+		id
+		location
+		description
+		companyName
+		lat
+		lng
+		}
+	}
 `;
 
 class PostViewer extends React.Component {
-  state = {
-    
-  }
+	state = {
 
-  render() {
-    const expandCard = (e) => {
+	}
 
-      // only expand card if click is NOT within Google map
-      if(e.target.parentNode.parentNode.parentNode.parentNode.className !== 'googleMap') {
-        this.setState({[e.currentTarget.id]: !this.state[e.currentTarget.id]});
-      }
-    }
+	render() {
+		const expandCard = (e) => {
+			// only expand card if click is NOT within Google map
+			if(e.target.parentNode.parentNode.parentNode.parentNode.className !== 'googleMap') {
+				this.setState({[e.currentTarget.id]: !this.state[e.currentTarget.id]});
+			}
+		}
 
-    return (
-      <Query 
-        query={GET_POSTS} 
-        variables={{ location: this.props.location, dayOfWeek: this.props.dayOfWeek}}
-        onCompleted={data => data.posts.forEach(post => this.setState({[post.id]: false}))}>
-          {({ loading, data }) => !loading && (
-              <div>
-                {/* Deals returned tagline */}
-                {data.posts.length > 0 ? (<h4 id="tagline">{this.props.location.length > 0 ? data.posts.length + ' d' : 'D'}eal{data.posts.length > 1 && 's'} {this.props.location.length > 0 && 'in '}<span className="location">{this.props.location}</span> for {this.props.dayOfWeek}</h4>) : (<h4 id="tagline">No results for '{this.props.location}'. Try again?</h4>)}
-                <div id="card-wrapper">
-                  {data.posts.map(post => {
-                    return (
-                      <Spring
-                      from={{opacity: 0, marginTop: -15}}
-                      to={{opacity:1, marginTop: 0}}
-                      key={post.id}>
-                        {props => (
-                          <div onClick={expandCard} className="card" style={props} key={post.id} id={post.id}>
-                            <div className="info">
-                              <h2 className="description">{post.description}</h2>
-                              <h3 className="companyName">{post.companyName}</h3>
-                              <h4 className="location"><i className="fas fa-map-marker-alt" style={{paddingRight: '.5rem'}}></i>{post.location}</h4>
-                              <h4 className="address">{post.address}</h4>
-                            </div>
-                            <div className="directions">
-                              <i className="fas fa-directions"></i>
-                            </div>
+		return (
+			<Query
+				query={GET_POSTS}
+				variables={{ location: this.props.location, dayOfWeek: this.props.dayOfWeek}}
+				onCompleted={data => data.posts.forEach(post => this.setState({[post.id]: false}))}>
+				{({ loading, data }) => !loading && (
+					<div>
+						{data.posts.length > 0 ? (<h4 id="tagline">{this.props.location.length > 0 ? data.posts.length + ' d' : 'D'}eal{data.posts.length > 1 && 's'} {this.props.location.length > 0 && 'in '}<span className="location">{this.props.location}</span> for {this.props.dayOfWeek}</h4>) : (<h4 id="tagline">No results for '{this.props.location}'. Try again?</h4>)}
+						<div id="card-wrapper">
+							{data.posts.map(post => {
+								return (
+									<Spring
+										from={{opacity: 0, marginTop: -15}}
+										to={{opacity:1, marginTop: 0}}
+										key={post.id}>
 
-                            {!this.state[post.id] &&                             <div className="more">
-                              <i className="fas fa-angle-double-down"></i><em><h6>more</h6></em>
-                            </div> }
-
-
-
-                            {this.state[post.id] ? 
-                              <Map 
-                                googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyAeR4JJANXM7oOYOzhXYlrcQeJ5y0FKRw4&v=3.exp&libraries=geometry,drawing,places"
-                                loadingElement={<div className='googleMap' style={{ height: `100%` }} />}
-                                containerElement={<div className='googleMap' />}
-                                mapElement={<div className='googleMap' style={{ height: `100%` }} />}
-                                lat={post.lat}
-                                lng={post.lng}
-                              /> 
-                              : <></>}
-
-                          </div>
-                        )}
-                      </Spring>
-                      
-                    )
-                  })}
-                </div>
-              </div>
-          )}
-      </Query>
-    );
-  }
-
+										{props => (
+											<div onClick={expandCard} className="card" style={props} key={post.id} id={post.id}>
+												<div className="defaultCard">
+													<div className="info">
+														<h2 className="description">{post.description}</h2>
+														<h3 className="companyName">{post.companyName}</h3>
+														<h4 className="location"><i className="fas fa-map-marker-alt" style={{paddingRight: '.5rem'}}></i>{post.location}</h4>
+														<h4 className="address">{post.address}</h4>
+													</div>
+													<div className="directions">
+														<i className="fas fa-directions"></i>
+													</div>
+												</div>
+												
+												{this.state[post.id] ?
+													<Map
+														googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyAeR4JJANXM7oOYOzhXYlrcQeJ5y0FKRw4&v=3.exp&libraries=geometry,drawing,places"
+														loadingElement={<div className='googleMap' style={{ height: `100%` }} />}
+														containerElement={<div className='googleMap' />}
+														mapElement={<div className='googleMap' style={{ height: `100%` }} />}
+														lat={post.lat}
+														lng={post.lng}
+													/> : 
+													<div className="more">
+														<i className="fas fa-angle-double-down"></i><em><h6>more</h6></em>
+													</div>
+												}
+											</div>
+										)}
+									</Spring>
+								)
+							})}
+						</div>
+					</div>
+				)}
+			</Query>
+		);
+	}
 }
 
 export default PostViewer;
